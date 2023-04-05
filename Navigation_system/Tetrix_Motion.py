d3598ada2f65792f1bac60c636d7e334d4fbf673
Harikrishnan M <90817926+The-Kriz@users.noreply.github.com>
2023-04-05 15:55:33 +0530

    Update Tetrix_Motion.py

--- Navigation_system/Tetrix_Motion.py
+++ Navigation_system/Tetrix_Motion.py
@@ -8,97 +8,6 @@ Right     = 3
 Backward  = 4
 '''
 
-#
-# def motionPlan(path):
-#     i = 0
-#     bot_movements = []
-#     if path[i] == "Left":
-#         bot_movements.append("2")
-#     elif path[i] == "Right":
-#         bot_movements.append("3")
-#     elif path[i] == "Backward":
-#         bot_movements.append("4")
-#
-#     while True:
-#         if bot_movements[i] != "4":
-#             bot_movements.append("1")
-#             i += 1
-#         if i >= len(path):
-#             if path[i-1] == "Left":
-#                 bot_movements.append("3")
-#             elif path[i-1] == "Right":
-#                 bot_movements.append("2")
-#             break
-#         if path[i-1] != path[i] or i < len(path):
-#             if path[i-1] == "Left" and path[i] == "Forward":
-#                 bot_movements.append("3")
-#             elif path[i-1] == "Left" and path[i] == "Right":
-#                 bot_movements.append("3")
-#                 bot_movements.append("3")
-#             elif path[i-1] == "Right" and path[i] == "Forward":
-#                 bot_movements.append("2")
-#             elif path[i-1] == "Right" and path[i] == "Left":
-#                 bot_movements.append("2")
-#                 bot_movements.append("2")
-#             elif path[i-1] == "Forward" and path[i] == "Right":
-#                 bot_movements.append("3")
-#             elif path[i-1] == "Forward" and path[i] == "Left":
-#                 bot_movements.append("2")
-#     return bot_movements
-
-#
-#
-# def motionPlan(path):
-#     i = 0
-#     bot_movements = []
-#     if path[i] == "Left":
-#         bot_movements.append("2")
-#     elif path[i] == "Right":
-#         bot_movements.append("3")
-#     elif path[i] == "Backward":
-#         bot_movements.append("4")
-#     elif path[i] == "Forward":
-#         bot_movements.append("1")
-#     i += 1
-#     while True:
-#         if i >= len(path):
-#             break
-#         if path[i - 1] != path[i] or i < len(path):
-#             if path[i - 1] == "Left" and path[i] == "Forward":
-#                 bot_movements.append("3")
-#                 bot_movements.append("1")
-#             elif path[i - 1] == "Left" and path[i] == "Right":
-#                 bot_movements.append("3")
-#                 bot_movements.append("3")
-#                 bot_movements.append("1")
-#             elif path[i - 1] == "Right" and path[i] == "Forward":
-#                 bot_movements.append("2")
-#                 bot_movements.append("1")
-#             elif path[i - 1] == "Right" and path[i] == "Left":
-#                 bot_movements.append("2")
-#                 bot_movements.append("2")
-#                 bot_movements.append("1")
-#             elif path[i - 1] == "Forward" and path[i] == "Right":
-#                 bot_movements.append("3")
-#                 bot_movements.append("1")
-#             elif path[i - 1] == "Forward" and path[i] == "Left":
-#                 bot_movements.append("2")
-#                 bot_movements.append("1")
-#             elif path[i - 1] == "Backward" and path[i] == "Left":
-#                 bot_movements.append("2")
-#                 bot_movements.append("1")
-#             elif path[i - 1] == "Backward" and path[i] == "Right":
-#                 bot_movements.append("3")
-#                 bot_movements.append("1")
-#             elif path[i - 1] == path[i]:
-#                 if path[i] == "Backward":
-#                     bot_movements.append("4")
-#                 else:
-#                     bot_movements.append("1")
-#             i += 1
-#     return bot_movements
-#
-
 
 def motionPlan(path):
     i = 0
@@ -180,12 +89,14 @@ def motionPlan(path):
 
             elif path[i - 1] == path[i]:
                 if path[i] == "Backward":
-                    bot_movements.append("4")
+                    if botFace == "SOUTH":
+                        bot_movements.append("1")
+                    else:
+                        bot_movements.append("4")
                 else:
                     bot_movements.append("1")
         i += 1
-    reversedMovement = bot_movements[::-1]
-    return reversedMovement
+    return bot_movements
 
 
 def sendMovement(bot_path, btModule):
@@ -200,14 +111,18 @@ def turnOfTetrix(btModule):
 
 
 def btConnect():
-    # btModule = serial.Serial('/dev/tty.HC-05-DevB', 9600) # Linux
-    btModule = serial.Serial('COM17', 9600)   # Windows
-    if btModule.is_open:
-        print('Connected to BT module')
-    else:
+    try :
+        # btModule = serial.Serial('/dev/tty.HC-05-DevB', 9600) # Linux
+        btModule = serial.Serial('COM17', 9600)   # Windows
+        if btModule.is_open:
+            print('Connected to BT module')
+        else:
+            print('Failed to connect to BT module')
+        sleep(3)
+        return btModule
+    except:
         print('Failed to connect to BT module')
-    sleep(3)
-    return btModule
+        return None
 
 
 def btDisconnect(btModule):
