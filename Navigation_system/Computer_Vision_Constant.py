27adc9226407132d93bc9e9dab3ca07b2a64add2
Harikrishnan M <90817926+The-Kriz@users.noreply.github.com>
2023-04-05 15:57:16 +0530

    Add files via upload

---
+++ Navigation_system/Computer_Vision_Constant.py
@@ -0,0 +1,7 @@
+#Constants for grid cell
+
+gridArucoID = [0, 1, 2, 4]
+tetrixStart = 18 #4 #15
+tetrixHome = 17  #5  #20
+GridHoleMarkerID = [14, 17, 15, 10, 12] #[16, 17, 18, 19]
+# GridFrozenFloorMarkerID = [6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
