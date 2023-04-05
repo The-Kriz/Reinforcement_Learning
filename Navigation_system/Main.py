685f1e8329d30931125670d77d355f29b0cd3ef2
Harikrishnan M <90817926+The-Kriz@users.noreply.github.com>
2023-04-05 15:56:11 +0530

    Update Main.py

--- Navigation_system/Main.py
+++ Navigation_system/Main.py
@@ -1,22 +1,26 @@
-import cv2
-from Path import *
-from Computer_Vision import *
+from Computer_Vision_Constant import *
+from Aruco_Perspective_Crop import *
 from Tetrix_Motion import *
+from Path import *
+import cv2
 
-## no HC05 part
 
-image = cv2.imread('Testing_Images/aruco6.jpg')
+image = cv2.imread('Testing_Images/aruco24.jpg')
 cv2.imshow("Original image", image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-
-cropped_image = ArucoCropImage(image)
+#
+cropped_image = ArucoCropImage(image, CustomOrder=gridArucoID)
 cv2.imshow('Cropped Image', cropped_image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
 ids = DetectAruco(cropped_image)
-val = SplitImage(cropped_image)
+val, cellImage = SplitImage(cropped_image, imageReturn=True)
+cv2.imshow("cell", cellImage)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
 grid = ArucoIdToGrid(val)
 print(grid)
 
@@ -28,5 +32,6 @@ movement = motionPlan(path)
 print(movement)
 
 btModule = btConnect()
-sendMovement(movement, btModule)
-btDisconnect(btModule)
+if btModule:
+    sendMovement(movement, btModule)
+    btDisconnect(btModule)
